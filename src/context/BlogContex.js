712b51338af45e0730df0e31dedb20bfbc9f7055
Rohit Balage <thatcomputerboy26@gmail.com>
2024-12-15 22:00:45 -0500

    added reducer in Blog context

--- src/context/BlogContex.js
+++ src/context/BlogContex.js
@@ -1,21 +1,31 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
 
 const BlogContext =React.createContext();
 
+// used reducer here:::::::
+const blogPostReducer = ( state, action) =>
+{
+switch(action.type)
+{
+  case 'add_blogpost':
+    return  [...state, {title: `Blog Post  #${state.length+1}`}];
+    default:
+        return state;
+}
+};
+
 export const BlogProvider =  ({ children })  => {
 
-   const [blogPost, setBlogPosts] = useState([]);
+   const [blogPost, dispatch] = useReducer(blogPostReducer, []);
 
 
-   const addBlogPost = () => 
-   {
-    //create a new array and add blogPost inside of it.
-    setBlogPosts([ ...blogPost, {title:`Blog post #${blogPost.length +1}`
-    } ]);
-   }
+const addBlogPost = () =>
+{
+dispatch({type: 'add_blogpost' });
+};
 
     return (
-    <BlogContext.Provider value={{data: blogPost, addBlogPost: addBlogPost}}>{ children}</BlogContext.Provider>
+    <BlogContext.Provider value={{data: blogPost, addBlogPost}}>{ children}</BlogContext.Provider>
     );
 };
 
