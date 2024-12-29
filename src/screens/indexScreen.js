874c066d32164be3464eae034447a24ec8c4a136
Rohit Balage <thatcomputerboy26@gmail.com>
2024-12-29 12:28:54 -0500

    changed view of flatlist

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import { View, Text, StyleSheet, FlatList, Button} from "react-native";
 import { Context} from "../context/BlogContex";
-
+import Feather from '@expo/vector-icons/Feather';
 
 
 const IndexScreen = ()  =>{
@@ -9,20 +9,40 @@ const IndexScreen = ()  =>{
 const {state, addBlogPost} = useContext(Context);
 
 return (<View>
-<Text>
-Index Screen
-</Text>
 <Button title="Add Post" onPress={() => addBlogPost()} />
 <FlatList
 data={state}
 keyExtractor={blogPost => blogPost.title}
 renderItem={({item}) =>{
-    return <Text>{item.title} </Text>
+    return <View style={styles.row}>
+        <Text style={styles.title}>{item.title} </Text> 
+    <Feather style={styles.icon} name="trash"/>
+    </View>
 }}
 />
 </View>);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+row:
+{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: 'grey'
+},
+title:
+{
+    fontSize: 18,  
+},
+icon:
+{
+  fontSize: 24  
+}
+
+});
 
 export default IndexScreen;
