40ccec0355421d7b32a9940d877a62661d29fec9
Rohit Balage <thatcomputerboy26@gmail.com>
2025-01-11 11:38:59 -0500

    saving a new post

--- src/context/BlogContex.js
+++ src/context/BlogContex.js
@@ -1,3 +1,4 @@
+import { act } from "react";
 import createDataContext from "./createDataContext";
 
 // used reducer here:::::::
@@ -10,7 +11,8 @@ const blogReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 999999),
-          title: `Blog Post  #${state.length + 1}`,
+          title: action.payload.title,
+          content:action.payload.content
         },
       ];
     default:
@@ -19,8 +21,8 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: "add_blogpost" });
+  return (title, content) => {
+    dispatch({ type: "add_blogpost", payload: {title,content}});
   };
 };
 
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -6,13 +6,15 @@ const CreateScreen = ({ navigation }) => {
 
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const {addBlogPost } =  useContext(Context);
   return (
     <View>
       <Text style={styles.label} >Enter Title</Text>
       <TextInput style={styles.input} value={title}  onChangeText={(text) => setTitle(text)}/>
        <Text style={styles.label}>Enter Content:</Text> 
        <TextInput style={styles.input} value={content} onChangeText={(text)=>setContent(text)}/>
-        <Button title="Add Blog Post"> </Button>
+        <Button title="Add Blog Post"
+        onPress={() =>addBlogPost(title,content)}/>
     </View>
   );
 };
