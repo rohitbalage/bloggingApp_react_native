c3ad186e68a1d032f54449cd8c4634d18d31612d
Rohit Balage <thatcomputerboy26@gmail.com>
2025-01-16 14:26:50 -0500

    editing in reducer

--- src/context/BlogContex.js
+++ src/context/BlogContex.js
@@ -4,6 +4,20 @@ import createDataContext from "./createDataContext";
 // used reducer here:::::::
 const blogReducer = (state, action) => {
   switch (action.type) {
+  case "edit_blogpost":
+    return state.map((blogPost)=>
+    {
+return blogPost.id === action.payload.id ? action.payload : blogPost;
+//  if(blogPost.id === action.payload.id)
+//       {
+//         return action.payload;
+//       }
+//       else
+//       {
+//         return blogPost;
+//       }
+    });
+
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "add_blogpost":
